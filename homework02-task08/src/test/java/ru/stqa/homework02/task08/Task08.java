90672344a51e7094b4abd43456cd7ec9958c86b5
Aleksey <alekseygukov.mp@gmail.com>
2020-11-23 20:13:27 +0300

    added homework02-task08

---
+++ homework02-task08/src/test/java/ru/stqa/homework02/task08/Task08.java
@@ -0,0 +1,56 @@
+package ru.stqa.homework02.task08;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Task08 {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeEach
+    void beforeEach() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @AfterEach
+    void afterEach() {
+        driver.quit();
+    }
+
+    @Test
+    void every_product_on_main_page_should_have_sticker() {
+        driver.navigate().to("http://localhost/litecart/en/");
+        waitForPageLoad();
+        List<WebElement> elements = driver.findElements(By.cssSelector("a.link > div.image-wrapper"));
+        assertThat(elements).isNotEmpty();
+        for (WebElement element : elements) {
+            List<WebElement> stickers = element.findElements(By.cssSelector("div[class ^= sticker]"));
+            assertThat(stickers).isNotEmpty().hasSize(1);
+        }
+    }
+
+    private void waitForPageLoad() {
+        ExpectedCondition<Boolean> pageLoadCondition = new
+                ExpectedCondition<Boolean>() {
+                    public Boolean apply(WebDriver driver) {
+                        return ((JavascriptExecutor)driver).executeScript("return document.readyState").equals("complete");
+                    }
+                };
+        wait.until(pageLoadCondition);
+    }
+}
